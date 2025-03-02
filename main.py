4bb4bfd2d43252c0ded8c355122f6995d2e1d2a7
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-03-02 09:25:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Multiplication Table
+
+num = int(input("Enter the Number: "))
+
+print(num, "X 1 = ", num * 1)
+print(num, "X 2 = ", num * 2)
+print(num, "X 3 = ", num * 3)
+print(num, "X 4 = ", num * 4)
+print(num, "X 5 = ", num * 5)
+print(num, "X 6 = ", num * 6)
+print(num, "X 7 = ", num * 7)
+print(num, "X 8 = ", num * 8)
+print(num, "X 9 = ", num * 9)
+print(num, "X 10 = ", num * 10)
